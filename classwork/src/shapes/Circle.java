ef0617935a1ef29efbe92323f631e7ea0514e18f
Thomas <tzhou8026@bths.edu>
2017-10-18 12:47:15 -0400

    Update Circle.java

--- classwork/src/shapes/Circle.java
+++ classwork/src/shapes/Circle.java
@@ -1,18 +1,37 @@
 package shapes;
 
+/**
+ * @author Mr Levin Created 10/16/2017 Lab 2.1 shapes
+ *
+ */
 public class Circle implements Shape{
-	private static double pi = 3.14;
-	private double radius;
-	public Circle(double rad)
-	{
-		this.radius = rad;
-	}
-	public double area()
-	{
-		return pi * this.radius * this.radius;
-	}
-	public double perimeter()
-	{
-		return 2 * this.radius * pi;
-	}
+	
+    //Fields
+    private int radius;
+    private static double pi = 3.14;
+
+    /**
+     * Constructor method
+     * @param radius
+     */
+    public Circle(int radius){
+    	this.radius = radius;
+    }
+    
+    @Override
+    public double calculateArea() {
+    	return pi * this.radius * this.radius;
+    }
+
+    @Override
+    public double calculatePerimeter()
+    {
+    	return 2 * this.radius * pi;
+    }
+
+    @Override
+    public String toString()
+    {
+    	return "Circle Radius: " + radius + " Area: " + this.calculateArea() + " Perimeter: " + this.calculatePerimeter();
+    }
 }
