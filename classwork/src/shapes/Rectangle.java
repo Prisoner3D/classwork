dcb3fa53694c12ac7fcbfdc07ad5456ee78b729c
Thomas <tzhou8026@bths.edu>
2017-10-18 13:06:26 -0400

    Update Rectangle.java

--- classwork/src/shapes/Rectangle.java
+++ classwork/src/shapes/Rectangle.java
@@ -1,19 +1,34 @@
 package shapes;
 
-public class Rectangle implements Shape {
-	private double length;
-	private double width;
-	public Rectangle(double length, double width)
-	{
-		this.length = length;
-		this.width = width;
-	}
-	public double perimeter()
-	{
-		return (length * 2 + width * 2);
-	}
-	public double area() 
-	{
-		return length * width;
-	}
+/**
+ * @author Mr Levin Created 10/16/2017 Lab 2.1 shapes
+ *
+ */
+public class Rectangle implements Shape{
+	
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+
+    @Override
+    public double calculateArea() {
+    	return length * width;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return (length * 2 + width * 2);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Rectangle Width: " + width + " Length: " + length + " Area: " + this.calculateArea() + " Perimeter: " + this.calculatePerimeter();
+    }
 }
+
