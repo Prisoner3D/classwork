ba8e6b05106a6f1784569c3ae76dbb7d68eefe82
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-12-14 12:37:09 -0500

    Fixed and finished.

--- classwork/src/lab4_1/Deck.java
+++ classwork/src/lab4_1/Deck.java
@@ -1,11 +1,14 @@
 package lab4_1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class Deck {
 
-	private ArrayList<Card> unDealt;
-	private ArrayList<Card> Dealt;
+	private ArrayList<Card> unDealt = new ArrayList<Card>();
+	private ArrayList<Card> Dealt = new ArrayList<Card>();
 	
 	public Deck(String[] ranks, String[] suits, int[] pointValues) {
 		for (int rank = 0; rank < ranks.length; rank++)
@@ -20,6 +23,30 @@ public class Deck {
 
 	public boolean isEmpty()
 	{
-		return unDealt.size() + Dealt.size() == 0;
+		return unDealt.isEmpty();
+	}
+	
+	public Card deal()
+	{
+		if (!unDealt.isEmpty())
+		{
+			Card dealingCard = unDealt.get(0);
+			unDealt.remove(0);
+			Dealt.add(dealingCard);
+			return dealingCard;
+		}
+		return null;
+	}
+	
+	public void shuffle()
+	{
+		unDealt.addAll(Dealt);
+		Dealt.clear();
+		Random random = new Random();
+		for (int k = 51; k >= 1; k--)
+		{
+			int r = random.nextInt(k);
+			Collections.swap(unDealt, k, r);
+		}
 	}
 }
---
+++ classwork/src/lab4_1/Runner.java
@@ -0,0 +1,18 @@
+package lab4_1;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		String[] ranks = new String[] {"Jack", "Queen", "King", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+		String[] suits = new String[] {"Hearts", "Clubs", "Diamond", "Spades"};
+		int[] pointValues = new int[] {11,12,13,1,2,3,4,5,6,7,8,9,10};
+		Deck deck = new Deck(ranks, suits, pointValues);
+		deck.shuffle();
+		for (int i = 0; i < 52; i++)
+		{
+			Card current = deck.deal();
+			System.out.println(current);
+		}
+	}
+
+}
