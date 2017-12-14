5e45f6b5855263976f131c8dffb84de0ed3cf078
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-12-14 13:13:24 -0500

    Comments

--- classwork/src/lab4_1/Card.java
+++ classwork/src/lab4_1/Card.java
@@ -6,24 +6,47 @@ public class Card {
 	private String suit;
 	private int pointValue;
 	
+	/**
+	 * Card constructor
+	 * @param rank The rank of the card
+	 * @param suit The suit of the card
+	 * @param pointValue The 
+	 */
 	public Card(String rank, String suit, int pointValue) {
 		this.rank = rank;
 		this.suit = suit;
 		this.pointValue = pointValue;
 	}
 
+	/**
+	 * Returns the rank of the card.
+	 * @return rank The rank of the card.
+	 */
 	public String getRank() {
 		return rank;
 	}
 
+	/**
+	 * Returns the suit of the card.
+	 * @return suit The suit of the card.
+	 */
 	public String getSuit() {
 		return suit;
 	}
 
+	/**
+	 * Returns the pointValue of the card.
+	 * @return pointValue The point value of the card.
+	 */
 	public int getPointValue() {
 		return pointValue;
 	}
 	
+	/**
+	 * Checks if two cards are equal
+	 * @param otherCard The card checked with.
+	 * @return equal Are the cards equal
+	 */
 	public boolean equals(Card otherCard)
 	{
 		return this.suit.equals(otherCard.getSuit()) && this.rank.equals(otherCard.getSuit()) && this.pointValue == otherCard.getPointValue();
--- classwork/src/lab4_1/Deck.java
+++ classwork/src/lab4_1/Deck.java
@@ -2,34 +2,40 @@ package lab4_1;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
+/**
+ * A deck class which contains cards.
+ * @author Thomas Zhou
+ *
+ */
 public class Deck {
 
-	private ArrayList<Card> unDealt = new ArrayList<Card>();
 	private ArrayList<Card> Dealt = new ArrayList<Card>();
-	
+	private ArrayList<Card> unDealt = new ArrayList<Card>();
+
+	/**
+	 * Deck constructor
+	 * 
+	 * @param ranks The possible ranks for the deck.
+	 * @param suits The possible suits for the deck.
+	 * @param pointValues The corresponding point values of the ranks.
+	 */
 	public Deck(String[] ranks, String[] suits, int[] pointValues) {
-		for (int rank = 0; rank < ranks.length; rank++)
-		{
-			for (int suit = 0; suit < suits.length; suit++)
-			{
+		for (int rank = 0; rank < ranks.length; rank++) {
+			for (int suit = 0; suit < suits.length; suit++) {
 				Card card = new Card(ranks[rank], suits[suit], pointValues[rank]);
 				unDealt.add(card);
 			}
 		}
 	}
 
-	public boolean isEmpty()
-	{
-		return unDealt.isEmpty();
-	}
-	
-	public Card deal()
-	{
-		if (!unDealt.isEmpty())
-		{
+	/** 
+	 * Deals a card from unDealt and moves it to Dealt.
+	 * @return card The card dealt.
+	 */
+	public Card deal() {
+		if (!unDealt.isEmpty()) {
 			Card dealingCard = unDealt.get(0);
 			unDealt.remove(0);
 			Dealt.add(dealingCard);
@@ -37,16 +43,33 @@ public class Deck {
 		}
 		return null;
 	}
+
+	/**
+	 * Checks if the deck is empty.
+	 * @return empty
+	 */
+	public boolean isEmpty() {
+		return unDealt.isEmpty();
+	}
 	
-	public void shuffle()
-	{
+	/**
+	 * Shuffles the deck
+	 */
+	public void shuffle() {
 		unDealt.addAll(Dealt);
 		Dealt.clear();
 		Random random = new Random();
-		for (int k = 51; k >= 1; k--)
-		{
+		for (int k = 51; k >= 1; k--) {
 			int r = random.nextInt(k);
 			Collections.swap(unDealt, k, r);
 		}
+	}
+
+	/**
+	 * The size of the deck
+	 * @return
+	 */
+	public int size() {
+		return unDealt.size();
 	}
 }
